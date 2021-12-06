0eeb9ad9b38724775c7ed51c4bdb70ef96bc2b5e
shelly <shelly@deepnatural.ai>
2021-12-07 04:10:12 +0900

    Updates

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { useProduct } from './components/useProduct';
+import Product from './components/Product.vue'
+import ProductSetting from './components/ProductSetting.vue';
+
+const { loadProduct } = useProduct()
+
+loadProduct()
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <product />
+      <hr />
+      <product-setting></product-setting>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-width: 1000px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 1400px;
+}
+
+hr {
+  width: 100%;
+  border: none;
+  height: 1px;
+  background: lightgray;
+  margin: 20px 0;
+}
+
+</style>
---
+++ src/components/ProductDetailSetting.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="setting-group">
+    <h2 class="title">Product Description Settings</h2>
+    <div class="setting">
+      <label for="name">name</label> 
+      <div class="input-box">
+        <input 
+          id="name"
+          type="text" 
+          v-model="form.name"
+          autocomplete="off"
+        />
+      </div>
+    </div>
+    <div class="setting">
+      <label for="summary">
+        summary
+      </label>
+      <div class="input-box">
+        <input 
+          id="summary"
+          type="text" 
+          autocomplete="off"
+          v-model="form.summary"
+        />
+      </div>
+    </div>
+    <div class="button-wrapper">
+      <button
+        :disabled="!isModified.description"
+        class="save-button"
+        @click="save"
+      >
+        save
+      </button>
+    </div>
+  </div>
+  
+</template>
+
+<script setup>
+import { ref } from '@vue/reactivity';
+import { watch } from '@vue/runtime-core'
+import { useProduct } from './useProduct'
+import { useProductSetting } from './useProductSetting'
+const {
+  description, 
+  setProduct,
+  isModified,
+  setModificationStatus 
+} = useProduct()
+
+const {
+  description: form,
+  setForm
+} = useProductSetting()
+
+
+const section = 'description'
+
+watch(description.value, () => {
+  setForm(section, description.value)
+  
+  const stopWatch = watch(form, () => {
+    setModificationStatus(section, true)
+    stopWatch()
+  })
+})
+
+const save = () => { 
+  setProduct(section, form)
+  setModificationStatus(section, false)
+}
+</script>
+
+<style scoped>
+@import './styles.css';
+</style>
